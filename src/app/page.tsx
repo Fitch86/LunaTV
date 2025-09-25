be982f8813fcacb61d94a6b5bda7ffc55ad2215c
tom887812 <flushcar@hotmail.com>
2025-09-25 11:08:26 +0800

    fix type error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -432,11 +432,12 @@ function HomeClient() {
                               from='douban'
                               title={anime.name_cn || anime.name}
                               poster={
-                                anime.images.large ||
-                                anime.images.common ||
-                                anime.images.medium ||
-                                anime.images.small ||
-                                anime.images.grid
+                                anime.images?.large ||
+                                anime.images?.common ||
+                                anime.images?.medium ||
+                                anime.images?.small ||
+                                anime.images?.grid ||
+                                '/placeholder.jpg'
                               }
                               douban_id={anime.id}
                               rate={anime.rating?.score?.toFixed(1) || ''}
