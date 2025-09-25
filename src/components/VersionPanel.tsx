895220ca14db6ccd0ef7c68b0b381ed080f5838f
tom887812 <flushcar@hotmail.com>
2025-09-25 15:07:28 +0800

    fix: github address

--- src/components/VersionPanel.tsx
+++ src/components/VersionPanel.tsx
@@ -390,7 +390,7 @@ export const VersionPanel: React.FC<VersionPanelProps> = ({
                     </div>
                   </div>
                   <a
-                    href='https://github.com/Fitch86/LunaTV/tree/danmu'
+                    href='https://github.com/Fitch86/LunaTV/tree/newdanmu'
                     target='_blank'
                     rel='noopener noreferrer'
                     className='inline-flex items-center justify-center gap-2 px-3 py-2 bg-green-600 hover:bg-green-700 text-white text-xs sm:text-sm rounded-lg transition-colors shadow-sm w-full'
